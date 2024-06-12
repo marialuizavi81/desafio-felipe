7c1598200bb6c8d67a81e6dba71ef31e0232a924
marialuizavi81 <marialuizavitoria81@gmail.com>
2024-06-11 21:15:02 -0300

    terceira tentativa

---
+++ desafioXp/index.js
@@ -0,0 +1,10 @@
+console.log("aqui iremos apresentar o seu nivel de sp e seu correspondente, para comercarmos vamos entender quem voce é ? ");
+let = nive
+
+switch(true){
+
+    case ( nivelXp <=1000):
+    console.log("ferro");
+    break;
+
+}
